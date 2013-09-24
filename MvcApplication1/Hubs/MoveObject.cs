9dc0644bf59a087b77074b1e3a4a16ec52c1653f
GohanMF <GohanMF@hotmail.com>
2013-09-24 07:38:09 +0100

    changing how the object act when someone new enters (completes) Now when someone exists and reconnect his objects aren't lost

--- MvcApplication1/Hubs/MoveObject.cs
+++ MvcApplication1/Hubs/MoveObject.cs
@@ -20,36 +20,64 @@ namespace SignalHubs
             if (ConnectedUsers.Count(x => x.connectionId == id) == 0)
             {
                 ConnectedUsers.Add(new UserDetail { connectionId = id, userName = username });
-             //   Clients.Caller.onConnected(AllObjects);
+                //   Clients.Caller.onConnected(AllObjects);
                 if (username != string.Empty)
                 {
-                    Clients.All.newObject(username);
+                    var css = "shape draggable";
+
+
+
+                    if (AllObjects.Exists(x => x.id == username) == true)
+                    {
+
+                        Clients.Caller.loadallObjects(AllObjects);
+ 
+                        foreach (var obj in AllObjects.FindAll(x => x.id == username))
+                        {
+                            Clients.Others.newObject(obj.id, obj.cssClass, obj.mypossition);
+                        }
+                    }
+                    else
+                    {
+                        Clients.Caller.loadothersObjects(AllObjects);
+
+                        AllObjects.Add(new allObjects { id = username, cssClass = css });
+
+                        Clients.Others.newObject(username, css);
+                        Clients.Caller.myObject(username, css);
+
+                    }
                 }
             }
 
 
         }
-        
-        
+
+
         public void MoveObject(string id, int x, int y)
         {
             Clients.Others.objectMoved(id, x, y);
         }
 
-                
-        
+
+
         public void InsertobjectintoDiv(string idObject, string idDiv)
         {
             Clients.Others.objectInsertedDiv(idObject, idDiv);
         }
 
-        
-        
-        
-        
+
+
+
+
         public void especificPossition(string objid, string objcssClass, string objpossitionId)
         {
-            AllObjects.Add(new allObjects { id = objid, cssClass = objcssClass, mypossition = objpossitionId });
+
+           foreach (var item in AllObjects.FindAll(x => x.id == objid)){
+               item.mypossition = objpossitionId;
+
+            }
+
         }
 
         public override System.Threading.Tasks.Task OnDisconnected()
